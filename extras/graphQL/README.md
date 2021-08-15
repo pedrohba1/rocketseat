b020a984e30769ee30d5e354735ee3ca6fd771b2
pedrohba1 <pedro.almeida@ufu.br>
2021-08-15 15:40:20 -0300

    extra de graphQL

---
+++ extras/graphQL/README.md
@@ -0,0 +1,24 @@
+# Estudo de GraphQL do GoStack
+
+## Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Contributing](../CONTRIBUTING.md)
+
+## About <a name = "about"></a>
+
+Esse repositório foi um estudo sobre o funcionamento do graphQL usando node.
+
+### Prerequisites
+
+Você precisa instalar o mongodb locamente e iniciar ele manualmente com:
+
+```
+sudo service mongd start
+```
+
+## Usage <a name = "usage"></a>
+
+Em localhost:4000 você pode adicionar e buscar Comments.
---
+++ extras/graphQL/src/modules/comments/index.js
@@ -0,0 +1,37 @@
+import {
+  GraphQLString,
+  GraphQLList,
+  GraphQLObjectType,
+  GraphQLInputObjectType
+} from "graphql";
+import CommentType from "./CommentType";
+import { getComments, saveComment } from "./CommentLoader";
+
+export const queries = {
+  comments: {
+    type: GraphQLList(CommentType),
+    resolve: getComments
+  }
+};
+
+export const mutations = {
+  saveComment: {
+    type: CommentType,
+    resolve: saveComment,
+    args: {
+      input: {
+        type: new GraphQLInputObjectType({
+          name: "CommentInput",
+          fields: {
+            name: {
+              type: GraphQLString
+            },
+            content: {
+              type: GraphQLString
+            }
+          }
+        })
+      }
+    }
+  }
+};
